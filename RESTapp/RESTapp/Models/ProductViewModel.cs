a8a02b1ed354e17c86124e189161ad1191661f30
karol <karol@DESKTOP-K67V8IM>
2017-05-30 11:54:52 +0200

    Start over

---
+++ RESTapp/RESTapp/Models/ProductViewModel.cs
@@ -0,0 +1,18 @@
+﻿using RESTapp.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Client.Models
+{
+    public class ProductViewModel
+    {
+        [Key]
+        public ApplicationUser User { get; set; }
+        public List<Product> UserProducts { get; set; }
+        public List<ProductCategory> ProductCategories { get; set; }
+        public List<ProductSubcategory> ProductSubcategories { get; set; }
+    }
+}
---
+++ RESTapp/RESTapp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RESTapp.Startup))]
+namespace RESTapp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
